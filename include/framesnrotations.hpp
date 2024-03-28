ab5fb0b9c6d9368845424242aff0dce1a32a6c30
Rodolfo B. Negri <rodolfobnegri@yahoo.com.br>
2024-03-28 14:24:43 -0300

    update license

--- include/framesnrotations.hpp
+++ include/framesnrotations.hpp
@@ -1,3 +1,30 @@
+/*
+GNU General Public License with Academic Attribution
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+!!!!!!!!!!!!!!~~~ Additional Terms for Academic Use: ~~!!!!!!!!!!!!!!!!!!
+
+If this software is used in academic papers or publications, the authors
+are required to mention the original authorship in the text of the paper
+or publication, followed by the repository's URL.
+
+Example, suppose Software X was used for data analysis:
+"The data analysis was performed using Software X, developed by
+Rodolfo B. Negri~\footnote{[URL]}."
+*/
+
 #ifndef FRAMESNROTATIONS_HPP
 
 #include <array>
